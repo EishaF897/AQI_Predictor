f5c9896e19382cadce556fea699b231f89606a33
EishaF897 <eishafatima897@gmail.com>
2026-02-16 03:07:49 +0500

    Changes made

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from pipelines.inference_pipeline import get_predictions_json
 
 app = FastAPI()
