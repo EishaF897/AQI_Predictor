8b8492039f708eb296af230b6d4fdd4d7601a0bf
EishaF897 <eishafatima897@gmail.com>
2026-02-16 01:08:27 +0500

    Changes made

--- api.py
+++ api.py
@@ -3,6 +3,10 @@ from pipelines.inference_pipeline import get_predictions_json
 
 app = FastAPI()
 
+@app.get("/")
+def health():
+    return {"status": "API running"}
+    
 @app.get("/predict")
 def predict():
     return get_predictions_json()
