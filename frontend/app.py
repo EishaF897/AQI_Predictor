8d5e2d7a540faf3782aed609344bb72a85971eaa
EishaF897 <eishafatima897@gmail.com>
2026-02-16 21:44:19 +0500

    Changes made

--- frontend/app.py
+++ frontend/app.py
@@ -229,3 +229,16 @@ st.markdown(
 )
 
 st.caption("AQI Categories: 1=Good | 2=Moderate | 3=Poor | 4=Very Poor | 5=Hazardous")
+
+
+st.markdown("---")
+st.markdown(
+    """
+    <div style='text-align: center; font-size: 14px; color: gray;'>
+        © 2026 Eisha Fatima | Islamabad AQI Prediction System <br>
+        Built with Streamlit & Machine Learning
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
